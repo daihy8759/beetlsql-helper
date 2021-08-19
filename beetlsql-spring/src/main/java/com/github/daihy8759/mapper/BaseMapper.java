f07fe4ce8557341e8cb613734b0ab5766b31ed17
daihy <dai875939260@163.com>
2021-08-19 10:25:54 +0800

    👷 升级beetlsql至3.6.6-RELEASE

--- beetlsql-spring/src/main/java/com/github/daihy8759/mapper/BaseMapper.java
+++ beetlsql-spring/src/main/java/com/github/daihy8759/mapper/BaseMapper.java
@@ -13,28 +13,17 @@
  */
 package com.github.daihy8759.mapper;
 
-import com.github.daihy8759.mapper.internal.GetNameSpaceAMI;
 import com.github.daihy8759.query.PageRequest;
 import java.util.Map;
-import org.beetl.sql.core.SqlId;
 import org.beetl.sql.core.page.PageResult;
-import org.beetl.sql.mapper.annotation.AutoMapper;
+import org.beetl.sql.mapper.annotation.InheritMapper;
 
 public interface BaseMapper<T> extends org.beetl.sql.mapper.BaseMapper<T> {
 
-    @AutoMapper(GetNameSpaceAMI.class)
-    String getNamespace();
+  @InheritMapper
+  PageResult<T> selectPage(PageRequest pageRequest);
 
-    @SuppressWarnings(value = { "unchecked", "rawtypes" })
-    default PageResult<T> selectPage(PageRequest pageRequest) {
-        return getSQLManager().pageQuery(SqlId.of(getNamespace(), "selectPage"), getTargetEntity(),
-                pageRequest.getQueryParas(), pageRequest);
-    }
-
-    @SuppressWarnings("rawtypes")
-    default PageResult<Map> selectPageMap(PageRequest pageRequest) {
-        return getSQLManager().pageQuery(SqlId.of(getNamespace(), "selectPage"), Map.class, pageRequest.getQueryParas(),
-                pageRequest);
-    }
+  @InheritMapper
+  PageResult<Map> selectPageMap(PageRequest pageRequest);
 
 }
